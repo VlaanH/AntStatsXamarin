1f429e81268efb8d4b924c934b64ecfafb9f85d5
vlanh <vlan.h@yandex.ua>
2021-07-01 22:16:32 +0300

    Basic functionality RC1

---
+++ AntStats.Xamarin/AntStats.Xamarin/CustomItem/CustomItem.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AntStats.Xamarin
+{
+    public class CustomItem
+    {
+        public static Label AddLabel(string nameLabel)
+        {
+            Label labelCustom = new Label();
+        
+            
+            labelCustom.Text = "-";
+            labelCustom.FontSize = 16;
+            labelCustom.Margin = new Thickness(1);
+            labelCustom.VerticalOptions = LayoutOptions.Center;
+            labelCustom.HorizontalOptions = LayoutOptions.Center;
+            labelCustom.TextColor=Color.Azure;
+
+            
+            return labelCustom;
+        }
+
+
+
+
+    }
+}
---
+++ AntStats.Xamarin/AntStats.Xamarin/Data/AppStandartData.cs
@@ -0,0 +1,9 @@
+
+
+namespace AntStats.Avalonia
+{
+    public  class AppStandartData
+    {
+       // public static ISolidColorBrush StandartColorBrush=new SolidColorBrush(Color.FromRgb(30, 33, 36));
+    }
+}
