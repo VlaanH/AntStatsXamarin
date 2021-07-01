0a5dcb2d78291299466177e5a20319e0bac1287f
vlanh <vlan.h@yandex.ua>
2021-07-01 22:44:53 +0300

    Basic functionality RC3

---
+++ AntStats.Xamarin/AntStats.Xamarin/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace AntStats.Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+     
+
+
+            InitializeComponent();
+            
+            
+            Current.MainPage = new NavigationPage(new Stats());
+
+        }
+
+    }
+}
